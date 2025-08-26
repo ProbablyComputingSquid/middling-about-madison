df2e1114f91a517044fb6b359e9a3a81e3e52dcf
Jay Bolling <jwbolling001@gmail.com>
2025-08-26 10:26:18 -0500

    add a check point at the end + win music sound effect

--- capitol.gd
+++ capitol.gd
@@ -11,11 +11,10 @@ func _on_body_entered(body):
 	print("registered collision")
 	if body.is_in_group("player"):
 		print("player collision");
-		
+		$"../Label".text = "Thanks for playing!!"
 		sfx.play();
-		hide();
 		await sfx.finished;
-		queue_free();
+		get_tree().quit()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
